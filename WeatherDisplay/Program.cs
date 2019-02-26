66543cb78332c2af61ad6caa3e99bb6e8d6563b4
Benjamin Wilk <benjaminawilk@gmail.com>
2019-02-26 15:51:53 -0500

    ICAO data is now complete, and weather data is currently being pulled in.  I'm working on a multidimensional array to hold the weather data, but apparently C# doesn't like multidimensional String arrays!  This code will definitely be needing a lot of optimizations and comments added.

--- WeatherDisplay/Program.cs
+++ WeatherDisplay/Program.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Text.RegularExpressions;
 using System.Collections;
+using System.Globalization;
 
 namespace WeatherDisplay {
     class MainClass {
@@ -18,7 +19,8 @@ namespace WeatherDisplay {
             WeatherData wd = new WeatherData();
             new ObtainZipCode(wd);
             new ObtainICAO(wd);
-
+            new ObtainUserDate(wd);
+            new ObtainWeatherData(wd);
             Console.ReadKey();
         }
 
@@ -153,22 +155,63 @@ namespace WeatherDisplay {
             do {
                 iterationValue++;
             } while (icaoList[iterationValue].ToString().Substring(0).ToUpper() == "K");
-            Console.WriteLine("SetICAO Value: " + "" + icaoList[iterationValue]);
+           // Console.WriteLine("SetICAO Value: " + "" + icaoList[iterationValue]);
             wd.SetIcao("" + icaoList[iterationValue]);
         }
     }
 
-    class GetUserDate {
-        public GetUserDate() {
-
+    class ObtainUserDate {
+        public ObtainUserDate(WeatherData wd) {
+            String formattedDate = formatDate(GetDate());
+            wd.SetDate(formattedDate);
         }
 
-        public String getDate() {
+        public String GetDate() {
             Console.Write("Enter the date you would like to view(mm/dd/yyyy): ");
             return Console.ReadLine();
         }
 
-        private void formatDate() {
+        private String formatDate(String userDate) {
+            String dateFormat = "MM/dd/yyyy";
+            String newDateFormat = "yyyy/MM/dd";
+            DateTime parsedDate;
+            DateTime.TryParseExact(userDate, dateFormat, null,
+                                   DateTimeStyles.None, out parsedDate);
+            return parsedDate.ToString(newDateFormat);
+        }
+
+    }
+
+    class ObtainWeatherData {
+        public ObtainWeatherData(WeatherData wd) {
+            ParseWeatherData(DownloadWeatherData(wd));
+        }
+
+        private String getWeatherURL(WeatherData wd) {
+            return @"http://api.wunderground.com/history/airport/" + wd.GetIcao() + "/" + wd.GetDate() + "/DailyHistory.html?reqdb.zip=" + wd.GetZip();
+        }
+
+        private String DownloadWeatherData(WeatherData wd) {
+            String airportURL = getWeatherURL(wd);
+            HtmlWeb webData = new HtmlWeb();
+            var htmlDoc = webData.Load(airportURL);
+            var node = htmlDoc.DocumentNode.SelectSingleNode(".//body/div[@id='content-wrap']/div[@id='inner-wrap']/section[@id='inner-content']/div[@class='mainWrapper']/div[@class='row collapse']/div[@class='column large-12']/div[@id='observations_details']");
+            //Console.WriteLine(node.InnerText);
+            return "" + node.InnerText;
+        }
+
+        private void ParseWeatherData(String rawWeatherData) {
+            ArrayList dataWeather = new ArrayList();
+            String[] splitWeatherData = (rawWeatherData.Split('\n'));
+            for (int i = 0; i < splitWeatherData.Length; i++) {
+                if (String.IsNullOrEmpty(splitWeatherData[i].Trim()) == false) {
+                    dataWeather.Add(splitWeatherData[i].Trim());
+                }
+            }
+
+            for (int p = 0; p < dataWeather.Count; p++) {
+                Console.WriteLine("Weather data: <" + dataWeather[p] + ">");
+            }
 
         }
 
@@ -179,6 +222,7 @@ namespace WeatherDisplay {
         private String longFormName;
         private String ICAO;
         private String date;
+   //     private WeatherGraph wg;
 
         public String GetZip() {
             return this.zip;
@@ -212,7 +256,36 @@ namespace WeatherDisplay {
             return this.date;
         }
 
+   /*     public void initializeWeatherGraph(int row, int column) {
+            this.wg = new WeatherGraph(row, column);
+        }
+
+        public String getWeatherGraph(int row, int column) {
+            return this.wg.
+        }*/
+
     }
+
+ /*   class WeatherGraph {
+        String[,] WeatherData;
+
+        public WeatherGraph() {
+            WeatherData = new String[50,50];
+        }
+
+        public WeatherGraph(int row, int column) {
+            this.WeatherData = new ArrayList[row, column];
+        }
+
+        public void setWeatherGraphData(int row, int column, String dataToSet) {
+            this.WeatherData[row][column].Add(dataToSet);
+        }
+
+        public String GetWeatherGraphPlot(int row, int column) {
+            return this.WeatherData[row][column];
+        }
+
+    }*/
 
     class WeatherJson {
         [JsonProperty(PropertyName = "post code")]
